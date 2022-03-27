e5ce445fd01bc9b6f85fe13de00aff2b0d791975
Saira Tabassum <sairatabassum386@gmail.com>
2022-03-27 19:22:24 +0600

    commit

--- 08-Javascript&CSS/Assignment4/shared.js
+++ 08-Javascript&CSS/Assignment4/shared.js
@@ -1,13 +1,11 @@
 var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
-var selectPlanButton = document.querySelectorAll(".plan button");
+var selectPlanButtons = document.querySelectorAll(".plan button");
 var close = document.querySelector(".modal__action--negative");
 
-// console.log(backdrop);
-// console.dir(selectPlanButton);
-
-for (var i = 0; i < selectPlanButton.length; i++) {
-  selectPlanButton[i].addEventListener("click", function () {
+// console.dir(backdrop);
+for (var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener("click", function () {
     modal.style.display = "block";
     backdrop.style.display = "block";
   });
