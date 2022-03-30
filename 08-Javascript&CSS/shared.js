a2684f8b97d2fa6bf176e9bb5cdca7ccc0a09d78
Saira Tabassum <sairatabassum386@gmail.com>
2022-03-31 02:54:15 +0600

    nav

--- 08-Javascript&CSS/shared.js
+++ 08-Javascript&CSS/shared.js
@@ -2,6 +2,8 @@ var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
 var selectPlanButton = document.querySelectorAll(".plan button");
 var close = document.querySelector(".modal__action--negative");
+var btn = document.querySelector(".toggle-button");
+var nav = document.querySelector(".mobile-nav");
 
 // console.log(backdrop);
 // console.dir(selectPlanButton);
@@ -12,10 +14,16 @@ for (var i = 0; i < selectPlanButton.length; i++) {
     backdrop.style.display = "block";
   });
 }
-backdrop.addEventListener("click", closeModal);
-close.addEventListener("click", closeModal);
-
 function closeModal() {
   modal.style.display = "none";
   backdrop.style.display = "none";
+  nav.style.display = "none";
 }
+
+backdrop.addEventListener("click", closeModal);
+close.addEventListener("click", closeModal);
+
+btn.addEventListener("click", function () {
+  nav.style.display = "block";
+  backdrop.style.display = "block";
+});
