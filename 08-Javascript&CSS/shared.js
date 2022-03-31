55174a0a9c6d9a82d75846fa3e2f8f19014d328b
Saira Tabassum <sairatabassum386@gmail.com>
2022-03-31 18:54:43 +0600

    commit

--- 08-Javascript&CSS/shared.js
+++ 08-Javascript&CSS/shared.js
@@ -10,18 +10,28 @@ var nav = document.querySelector(".mobile-nav");
 
 for (var i = 0; i < selectPlanButton.length; i++) {
   selectPlanButton[i].addEventListener("click", function () {
-    modal.style.display = "block";
-    backdrop.style.display = "block";
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+
+    modal.classList.add("open");
+    backdrop.classList.add("open");
   });
 }
 function closeModal() {
-  modal.style.display = "none";
+  // modal.style.display = "none";
   backdrop.style.display = "none";
   nav.style.display = "none";
+
+  if (modal) {
+    modal.classList.remove("open");
+  }
+  backdrop.classList.remove("open");
 }
 
 backdrop.addEventListener("click", closeModal);
-close.addEventListener("click", closeModal);
+if (close) {
+  close.addEventListener("click", closeModal);
+}
 
 btn.addEventListener("click", function () {
   nav.style.display = "block";
